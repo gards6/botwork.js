586b90262c1af90e6fd99ef0eaa8c72dc43f4c3e
gards6 <39126935+gards6@users.noreply.github.com>
2018-07-05 14:15:28 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '601050150:AAGuDwMDfua5O7hEwLSXSVem2MV8ZRazamc';
+
+//polling function inneficient and unrealiable
+//7/5/18 1:35PM try webhook function so that telegram api sends a message
+// to our application each time a user sends a message in the chat
+//- much faster and reliable in message heavy groups
+
+
+
+//const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+
+
+
